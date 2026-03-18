c43ba9df32ca0b21b225c75e67fb04f3f9cf3ee1
Federico Taborda <fetaborda@mi.unc.edu.ar>
2026-03-18 17:21:37 -0300

    Cambios en plan controler

--- el-gym-back/src/controllers/planController.js
+++ el-gym-back/src/controllers/planController.js
@@ -26,7 +26,9 @@ const publicarPlan = async (req, res) => {
             alumnoId: alumno._id,
             adminId: req.user._id,
             esPlantilla: false,
+
             activo: true,
+
             sesiones
         });
 
